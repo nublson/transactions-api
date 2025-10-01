bff9b5d6e718bb9ec6f877719e1d4fc31b772f85
Nubelson Fernandes <me@nublson.com>
2025-10-01 06:45:28 +0100

    first queries

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import crypto from "node:crypto";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Testing transaction",
+      amount: 4000,
+    })
+    .returning("*");
 
-  return tables;
+  const transactions = await knex("transactions").select("*");
+
+  return transactions;
 });
 
 app
