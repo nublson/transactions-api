55f4cfcd4191044c160e746355a6e09c95a40f44
Nubelson Fernandes <me@nublson.com>
2025-10-01 18:11:25 +0100

    create transactions endpoint

---
+++ README.md
@@ -0,0 +1,12 @@
+# RF
+
+- [ ] User can create new transactions
+- [ ] User should be able to get the account resume
+- [ ] User should be able to list all the transactions
+- [ ] User should be able to read a unique transaction
+
+# RN
+
+- [ ] The transaction can be a credit, witch will sum the total amount or a debit witch will subtract
+- [ ] It should be possible to identify the user between requests
+- [ ] User can only view its own transactions
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  await knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Testing transaction",
-      amount: 4000,
-    })
-    .returning("*");
-
-  const transactions = await knex("transactions").select("*");
-
-  return transactions;
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app
